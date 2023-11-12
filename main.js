20bba3de1046d6902c2e759ba87808962777210f
William Davidson <wj.davidson@outlook.com>
2023-11-12 10:12:59 +0100

    mobile navigation, tickets scroll in to view

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-import anime from "animejs/lib/anime.es.js";
+const aside = document.querySelector("#aside");
+const asideInner = document.querySelector("#aside-inner");
+const menuBtns = document.querySelectorAll("#menu-btn");
+
+const ticketLink = document.querySelector("#ticket-link");
+const ticketsContainer = document.querySelector(`#tickets`);
+
+ticketLink.addEventListener("click", () => {
+  ticketsContainer.scrollIntoView({ behavior: "smooth" });
+});
+
+menuBtns.forEach((menuBtn) => {
+  menuBtn.addEventListener("click", () => {
+    if (aside.classList.contains("hidden")) {
+      // Open aside
+      aside.classList.replace("hidden", "absolute");
+      setTimeout(() => {
+        asideInner.classList.replace("translate-x-full", "translate-x-0");
+      }, 10);
+    } else {
+      // Close aside
+      asideInner.classList.replace("translate-x-0", "translate-x-full");
+      setTimeout(() => {
+        aside.classList.replace("absolute", "hidden");
+      }, 300);
+    }
+  });
+});
