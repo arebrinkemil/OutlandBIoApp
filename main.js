4432e0c40c6baf70e515c1107c717893145f0366
William Davidson <wj.davidson@outlook.com>
2023-11-16 14:03:51 +0100

    audi toggle on trailer, movieDetail.js

--- main.js
+++ main.js
@@ -151,38 +151,3 @@ cinemaCheck.forEach((checkBox) => {
     }
   });
 });
-
-function handleSlider(sliderId, nextId, prevId) {
-  let defaultTransform = 0;
-  const slider = document.getElementById(sliderId);
-  const next = document.getElementById(nextId);
-  const prev = document.getElementById(prevId);
-  const card = slider.querySelector(".flex-shrink-0");
-  const gapSize = 32; // gap mellan korten
-  let cardWidth = card.offsetWidth + gapSize;
-
-  function updateCardWidth() {
-    cardWidth = card.offsetWidth + gapSize;
-  }
-
-  window.addEventListener("resize", updateCardWidth);
-
-  function goNext() {
-    defaultTransform -= cardWidth;
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
-      defaultTransform = 0;
-    slider.style.transform = `translateX(${defaultTransform}px)`;
-  }
-
-  function goPrev() {
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform += cardWidth;
-    slider.style.transform = `translateX(${defaultTransform}px)`;
-  }
-
-  next.addEventListener("click", goNext);
-  prev.addEventListener("click", goPrev);
-}
-
-handleSlider("slider1", "next1", "prev1");
-handleSlider("slider2", "next2", "prev2");
