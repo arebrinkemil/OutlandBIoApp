2adde968666c222377c18a2d2a32ef8342b19f63
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-16 12:40:01 +0100

    responsive for mobile

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ const imgViewSection = document.querySelector("#img-view-section");
 const ticketDropdowns = document.querySelectorAll("#ticket-dropdown");
 const cinemaCheck = document.querySelectorAll("#cinema-check");
 
-ticketLink.addEventListener("click", () => {
-  ticketsContainer.scrollIntoView({ behavior: "smooth" });
-});
+if (ticketLink) {
+  ticketLink.addEventListener("click", () => {
+    ticketsContainer.scrollIntoView({ behavior: "smooth" });
+  });
+}
 
 menuBtns.forEach((menuBtn) => {
   menuBtn.addEventListener("click", () => {
@@ -149,3 +151,38 @@ cinemaCheck.forEach((checkBox) => {
     }
   });
 });
+
+function handleSlider(sliderId, nextId, prevId) {
+  let defaultTransform = 0;
+  const slider = document.getElementById(sliderId);
+  const next = document.getElementById(nextId);
+  const prev = document.getElementById(prevId);
+  const card = slider.querySelector(".flex-shrink-0");
+  const gapSize = 32; // gap mellan korten
+  let cardWidth = card.offsetWidth + gapSize;
+
+  function updateCardWidth() {
+    cardWidth = card.offsetWidth + gapSize;
+  }
+
+  window.addEventListener("resize", updateCardWidth);
+
+  function goNext() {
+    defaultTransform -= cardWidth;
+    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
+      defaultTransform = 0;
+    slider.style.transform = `translateX(${defaultTransform}px)`;
+  }
+
+  function goPrev() {
+    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+    else defaultTransform += cardWidth;
+    slider.style.transform = `translateX(${defaultTransform}px)`;
+  }
+
+  next.addEventListener("click", goNext);
+  prev.addEventListener("click", goPrev);
+}
+
+handleSlider("slider1", "next1", "prev1");
+handleSlider("slider2", "next2", "prev2");
