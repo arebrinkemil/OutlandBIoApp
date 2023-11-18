587eb0e7f336a493e0a78dfe81c855b4785a3d24
William Davidson <wj.davidson@outlook.com>
2023-11-18 16:15:34 +0100

    tickets select day

--- main.js
+++ main.js
@@ -13,9 +13,6 @@ const imgCarouselContainers = document.querySelectorAll(
 const carouselBtns = document.querySelectorAll("#carousel-btn");
 const imgViewSection = document.querySelector("#img-view-section");
 
-const ticketDropdowns = document.querySelectorAll("#ticket-dropdown");
-const cinemaCheck = document.querySelectorAll("#cinema-check");
-
 if (ticketLink) {
   ticketLink.addEventListener("click", () => {
     ticketsContainer.scrollIntoView({ behavior: "smooth" });
@@ -94,60 +91,6 @@ carouselBtns.forEach((carouselBtn) => {
       setTimeout(() => {
         imgViewSection.classList.replace("fixed", "hidden");
       }, 300);
-    }
-  });
-});
-
-ticketDropdowns.forEach((dropdown) => {
-  const dropdownContent = dropdown.querySelector("#dropdown-content");
-
-  dropdownContent.addEventListener("click", (ev) => {
-    ev.stopPropagation();
-  });
-
-  dropdown.addEventListener("click", () => {
-    dropdownContent.style.height =
-      dropdownContent.clientHeight === 0
-        ? `${dropdownContent.scrollHeight}px`
-        : 0;
-  });
-});
-
-cinemaCheck.forEach((checkBox) => {
-  const checkPath = checkBox.querySelector("#check-path");
-
-  checkBox.addEventListener("click", () => {
-    // Directions normal, revers, alternate.
-    const svgAnimation = (direction = "normal") => {
-      anime({
-        targets: checkPath,
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: "easeInOutSine",
-        duration: 300,
-        direction,
-      });
-    };
-
-    if (checkBox.classList.contains("border-outland-black")) {
-      checkPath.classList.toggle("opacity-0");
-      checkBox.classList.add("bg-outland-red-900");
-      checkBox.classList.replace(
-        "border-outland-black",
-        "border-outland-red-800"
-      );
-      setTimeout(() => {
-        checkPath.classList.toggle("opacity-0");
-        svgAnimation();
-      }, 200);
-    } else {
-      svgAnimation("reverse");
-      setTimeout(() => {
-        checkBox.classList.remove("bg-outland-red-900");
-        checkBox.classList.replace(
-          "border-outland-red-800",
-          "border-outland-black"
-        );
-      }, 200);
     }
   });
 });
